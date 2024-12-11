6fc18c11cbd071115cb4919d297b21f3e4643096
EC2 Default User <ec2-user@ip-172-31-43-178.ap-northeast-1.compute.internal>
2024-12-11 05:21:56 +0000

    レイアウト調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,13 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: [:guest_login] # 認証が必要なアクションを指定
+  before_action :authenticate_user!, except: [:guest_login, :destroy] # 認証が必要なアクションを指定
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  # ゲストログイン処理
-  def guest_login
-    user = User.find_or_create_by(email: 'guest@example.com') do |u|
-      u.password = SecureRandom.urlsafe_base64
-      u.name = "ゲストユーザー"
-    end
-    sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-  end
-
-  # マイページ表示
-  def show
-    # `set_user` メソッドで @user は既に設定されているので、ここで再度設定する必要はありません
-  end
-
-  # プロフィール編集
-  def edit
-  end
-
-  # プロフィール更新
-  def update
-    if @user.update(user_params)
-      # ゲストユーザーでない場合にのみ再ログインを行う
-      sign_in @user, bypass: true unless @user.email == 'guest@example.com'
-      redirect_to root_path, notice: 'プロフィールが更新されました！'
-    else
-      render :edit
-    end
-  end
 
   # ユーザー削除
   def destroy
     if current_user == @user # 自分自身のアカウントのみ削除できるようにする
       @user.destroy
       sign_out @user # ユーザー削除後、サインアウト
-      redirect_to root_path, notice: 'アカウントが削除されました。'
+      redirect_to new_user_registration_path, notice: 'アカウントが削除されました。'
     else
       redirect_to root_path, alert: '他のユーザーのアカウントは削除できません。'
     end
