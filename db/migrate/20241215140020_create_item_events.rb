fd074b04dd916b9526e5b4ca4f721430cd4f7a0d
EC2 Default User <ec2-user@ip-172-31-43-178.ap-northeast-1.compute.internal>
2024-12-21 09:06:53 +0000

    referencesにカラム記述方法変更

--- db/migrate/20241215140020_create_item_events.rb
+++ db/migrate/20241215140020_create_item_events.rb
@@ -1,19 +1,12 @@
 class CreateItemEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :item_events do |t|
-      t.integer :item_id, null: false # アイテムID（外部キー）
-      t.integer :event_id, null: false # イベントID（外部キー）
+      t.references :item, type: :bigint, null: false, foreign_key: true
+      t.references :event, type: :bigint, null: false, foreign_key: true
+
       t.integer :amount, null: false # 数量
 
       t.timestamps # 作成日時と更新日時（自動で追加される）
     end
-
-    # 外部キー制約を追加（アイテムIDとイベントID）
-    add_foreign_key :item_events, :items, column: :item_id
-    add_foreign_key :item_events, :events, column: :event_id
-
-    # アイテムIDとイベントIDにインデックスを追加
-    add_index :item_events, :item_id
-    add_index :item_events, :event_id
   end
 end
