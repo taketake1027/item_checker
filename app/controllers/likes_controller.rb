ea4a318d99d7728ef624c850d7b189ed73f285bf
EC2 Default User <ec2-user@ip-172-31-43-178.ap-northeast-1.compute.internal>
2024-12-22 13:52:15 +0000

    いいね機能

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,27 @@
+class LikesController < ApplicationController
+  before_action :set_event
+  before_action :set_post
+
+  def create
+    if @post.likers.exclude?(current_user)
+      @post.likes.create(user: current_user)
+    end
+    redirect_to event_path(@event, @post) # リダイレクト先を統一
+  end
+
+  def destroy
+    like = @post.likes.find_by(user: current_user)
+    like.destroy if like
+    redirect_to event_path(@event, @post) # リダイレクト先を統一
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+end
