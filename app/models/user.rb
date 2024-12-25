8e9bf80feffbc96548ab0b195779c6ee01420779
EC2 Default User <ec2-user@ip-172-31-43-178.ap-northeast-1.compute.internal>
2024-12-25 14:14:15 +0000

    role修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,27 +6,34 @@ class User < ApplicationRecord
   has_many :events, through: :groups
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  # ユーザー名のバリデーション（名前が空でないこと）
   validates :name, presence: true
-
-  # メールアドレスのバリデーション（空でないこと、形式が正しいこと、一意であること）
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
-  # ユーザー削除時に関連するグループ参加情報も削除
+  
   before_destroy :remove_from_groups
 
   enum role: { employee: 0, staff: 1, leader: 2 }
   
   def guest?
-    self.role == 'guest'  # roleがguestの場合をゲストと判断
+    self.role == 'guest'  
   end
-   # Devise の設定（ユーザー認証）
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  
+  def role_name
+    case role
+    when "employee"
+      "社員"
+    when "staff"
+      "スタッフ"
+    when "leader"
+      "リーダー"
+    else
+      "ゲスト"
+    end
+  end
+
   private
 
-  # ユーザーが削除される前に、関連するグループからそのユーザーを削除
   def remove_from_groups
     self.groups.each do |group|
       group.group_users.find_by(user_id: self.id)&.destroy
