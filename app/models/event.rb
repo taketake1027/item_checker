702a3eb8ab0b2fb4631b0136c3c4b75d3c4607d4
EC2 Default User <ec2-user@ip-172-31-43-178.ap-northeast-1.compute.internal>
2025-02-05 21:47:02 +0000

    脆弱性の修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,8 @@ class Event < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :items, dependent: :destroy
-  has_many :group_users  # 正しい中間テーブル名に修正
-  has_many :users, through: :group_users # 修正
+  has_many :group_users
+  has_many :users, through: :group_users
   has_many :event_requests, dependent: :destroy
 
   validate :end_date_after_start_date
@@ -14,6 +14,12 @@ class Event < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :location, presence: true
+
+   # イベントに参加しているかを確認
+  def user_participating?(user)
+    # イベントに参加しているか確認するためには、group_usersテーブルを通じて、グループに参加しているユーザーかどうかをチェック
+    self.group.users.include?(user)
+  end
 
   private
 
