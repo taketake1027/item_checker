f8f63276bf60d8e770784d7ed14768a73a41ccfb
EC2 Default User <ec2-user@ip-172-31-43-178.ap-northeast-1.compute.internal>
2025-02-19 01:35:58 +0000

    バリデーションの不要なメッセージ削除

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
-  belongs_to :group
+  belongs_to :group, optional: true # これを追加
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :items, dependent: :destroy
@@ -9,22 +9,20 @@ class Event < ApplicationRecord
   has_many :event_requests, dependent: :destroy
 
   validate :end_date_after_start_date
-  validates :name, presence: true
-  validates :introduction, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :location, presence: true
-  validates :group_id, presence: true
+
+  # バリデーションの順番を意図的に調整
+  validates :name, :introduction, :start_date, :end_date, :location, presence: true
+  validates :group_id, presence: true # これを最後に
 
   # イベントに参加しているかを確認
-def user_participating?(user)
-  # イベントが承認されたユーザーは、グループ参加者のチェックをスルー
-  if event_requests.exists?(user_id: user.id, status: 'approved')
-    return true
+  def user_participating?(user)
+    # 承認されたイベントリクエストがあれば参加とみなす
+    return true if event_requests.exists?(user_id: user.id, status: 'approved')
+
+    # 通常通り、グループ参加者のチェック
+    group.users.include?(user)
   end
-  # 通常通り、グループ参加者のチェック
-  self.group.users.include?(user)
-end
+
   private
 
   def end_date_after_start_date
